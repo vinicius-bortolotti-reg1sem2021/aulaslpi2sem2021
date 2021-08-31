d6570f3c95ad93c53fc3c8ae385c404bb296d476
vinicius-bortolotti-reg1sem2021 <89818744+vinicius-bortolotti-reg1sem2021@users.noreply.github.com>
2021-08-30 22:35:27 -0300

    Update Main.java

--- src/main/java/aulaslpi/Main.java
+++ src/main/java/aulaslpi/Main.java
@@ -10,16 +10,7 @@ public class Main {
 	final static Logger log = LoggerFactory.getLogger(Main.class);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Random r = new Random();
-		for(int i = 0; i < 100; i++) {
-			int result = r.nextInt(5);
-			if( result == 0) {
-				log.error("Deu 0, logo, deu ruim");
-			} else {
-				log.info("Deu bom " +  result);
-			}
-		}
-		
+		log.info("Vinícius Bortolotti");		
 		
 	}
 
