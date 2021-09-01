fa5613447fbb65eada9646eef72a72d5521b836e
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2021-08-31 22:27:31 -0300

    Update Main.java

--- src/main/java/aulaslpi/Main.java
+++ src/main/java/aulaslpi/Main.java
@@ -1,21 +1,38 @@
 package aulaslpi;
 
-import aulaslpi.modelos.Aluno;
+import java.util.Scanner;
+
+import aulaslpi.designpattern.factory.Bike;
+import aulaslpi.designpattern.factory.Carro;
+import aulaslpi.designpattern.factory.Moto;
+import aulaslpi.designpattern.factory.TiposDeVeiculo;
+import aulaslpi.designpattern.factory.Veiculo;
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
-public class Main {
+public class Main {	
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Aluno aluno = Aluno.builder()
-				.curso("aloha")
-				.rm(455)
-				.idade(45)
-				.build();
-		
-		log.info("Aloha");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Escolha um tipo");
+		String tipo = scan.next();
+		Veiculo meuVeiculo = factory(tipo.toUpperCase());
+		meuVeiculo.buzina();
+	
 	}
+	
+	public static Veiculo factory(String tipo) {
+		if(TiposDeVeiculo.BIKE.name().equals(tipo)) {
+			return new Bike();
+		} else if( TiposDeVeiculo.MOTO.name().equals(tipo)) {
+			return new Moto();
+		} else {
+			return new Carro();
+		}		
+	}	
 
 }
+
