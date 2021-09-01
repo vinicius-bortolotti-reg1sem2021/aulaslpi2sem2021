d1bad8cc3dfad99d7a6fbfd1b466529b08b7224f
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2021-08-31 22:25:34 -0300

    Create Bike.java

---
+++ src/main/java/aulaslpi/designpatterns/factory/Bike.java
@@ -0,0 +1,20 @@
+package aulaslpi.designpattern.factory;
+
+public class Bike implements Veiculo {
+
+	public void anda() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void freia() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void buzina() {
+		// TODO Auto-generated method stub
+		System.out.println("Buzina de Bike");
+	}
+
+}
